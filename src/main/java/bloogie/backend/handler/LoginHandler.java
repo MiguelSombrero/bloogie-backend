a561e13a2930bcad2d785c100325d0d41486a014
Miika Somero <miika.somero@gmail.com>
2020-02-22 19:20:28 +0200

    added login handler some filtering

--- src/main/java/bloogie/backend/handler/LoginHandler.java
+++ src/main/java/bloogie/backend/handler/LoginHandler.java
@@ -1,9 +1,11 @@
 
 package bloogie.backend.handler;
 
+import bloogie.backend.domain.Views;
 import bloogie.backend.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import org.springframework.http.codec.json.Jackson2CodecSupport;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -32,7 +34,9 @@ public class LoginHandler {
      */
     public Mono<ServerResponse> login(ServerRequest request) {
         return accountService.getAuthenticatedUser()
-                .flatMap(user -> ok().contentType(APPLICATION_JSON).bodyValue(user));
+                .flatMap(user -> ok().contentType(APPLICATION_JSON)
+                        .hint(Jackson2CodecSupport.JSON_VIEW_HINT, Views.Account.class)
+                        .bodyValue(user));
     }
     
     
